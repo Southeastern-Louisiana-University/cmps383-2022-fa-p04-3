af50ff1794f789cbf20346fe0385c299e5195eb2
dotbehrens <dotbehrens@gmail.com>
2022-09-11 18:23:32 -0500

    update entities

--- FA22.P04.Web/Features/Users/UserRole.cs
+++ FA22.P04.Web/Features/Users/UserRole.cs
@@ -1,11 +1,16 @@
 
 using FA22.P04.Web.Features.Users;
+using FA22.P04.Web.Features.Roles;
 namespace FA22.P04.Web.Features.UserRoles;
+
 
 public class UserRole
 {
-    public int Id { get; set; }
-    public User user { get; set; }
-    public UserRole Role { get; set; }
+    // public int Id { get; set; }
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
+    public User User { get; set; }
+    public Role Role { get; set; }
+
 
 }
