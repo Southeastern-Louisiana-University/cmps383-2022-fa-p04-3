27b9e01e4d51447c029cc5c9b681dfe43d88f973
dotbehrens <dotbehrens@gmail.com>
2022-09-11 22:27:33 -0500

    create UserDto

--- FA22.P04.Web/Features/Users/UserDto.cs
+++ FA22.P04.Web/Features/Users/UserDto.cs
@@ -1,8 +1,11 @@
-namespace FA22.P04.Web.Features.Login;
+using FA22.P04.Web.Features.UserRoles;
+namespace FA22.P04.Web.Features;
 
-public class LoginDto
+public class UserDto
 {
     public int Id { get; set; }
     public string? UserName { get; set; }
-    public string[]? Roles { get; set; }
+    public ICollection<UserRole> UserRoles { get; set; }
+
+    public string Password { get; set; }
 }
